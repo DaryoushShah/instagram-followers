9b07c238d83dd8d824c08cb632370491ed829ad8
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-01-06 19:31:17 -0800

    Implement array return in getFollowers() & add viewNotFollowingBack()

--- instagram-followers.js
+++ instagram-followers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const InstagramFollowers = (() => {
   /* Default filepaths */
-  const followersFilePath = './files/followers1.json';
+  const followersFilePath = './files/followers.json';
   const followingFilePath = './files/following.json';
   
   const _loadJSON = (filepath) => {
@@ -11,20 +11,36 @@ const InstagramFollowers = (() => {
       console.log('ERROR: Attempting to load JSON object from file not ending in \'.json\'');
       return null;
     }
+    
     /* Return object for opened JSON file */
     return JSON.parse(fs.readFileSync(filepath));
   }
 
   const _getFollowers = () => {
+    /* Load followers JSON */
     const followersJSON = _loadJSON(followersFilePath);
     if(followersJSON === null){
       console.log('ERROR: JSON file not found');
       return null;
     }
-    return followersJSON;
+
+    /* Get the relationships_and_followers array */
+    const relationships_and_followers = followersJSON['relationships_followers'];
+    
+    const followers = [];
+    /* Loop through the array and push values to followers */
+    for(let i = 0; i < relationships_and_followers.length; i++){
+      followers.push(relationships_and_followers[i]['string_list_data'][0]['value']);
+    }
+    return followers;
   }
 
+  const viewNotFollowingBack = () => {
+
+v  }
+
   return {
-    _getFollowers
+    viewNotFollowingBack
   }
 })();
+
