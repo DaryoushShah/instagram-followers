9d5d6b8ee5d0b2aa802f4e76f1e62397ebb7787c
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-01-06 19:07:16 -0800

    Add _getFollowers()

--- instagram-followers.js
+++ instagram-followers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const InstagramFollowers = (() => {
   /* Default filepaths */
-  const followersFilePath = './files/followers.json';
+  const followersFilePath = './files/followers1.json';
   const followingFilePath = './files/following.json';
   
   const _loadJSON = (filepath) => {
@@ -14,9 +14,17 @@ const InstagramFollowers = (() => {
     /* Return object for opened JSON file */
     return JSON.parse(fs.readFileSync(filepath));
   }
-  
+
+  const _getFollowers = () => {
+    const followersJSON = _loadJSON(followersFilePath);
+    if(followersJSON === null){
+      console.log('ERROR: JSON file not found');
+      return null;
+    }
+    return followersJSON;
+  }
+
   return {
-    
+    _getFollowers
   }
 })();
-
