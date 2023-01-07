4904ca86a2755f50becf8c7c2c00f78091207923
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-01-06 19:02:13 -0800

    Add _loadJSON()

--- instagram-followers.js
+++ instagram-followers.js
@@ -4,7 +4,17 @@ const InstagramFollowers = (() => {
   /* Default filepaths */
   const followersFilePath = './files/followers.json';
   const followingFilePath = './files/following.json';
-
+  
+  const _loadJSON = (filepath) => {
+    /* Check to ensure file is of type .json */
+    if(filepath.slice(filepath.length - 5) !== '.json'){
+      console.log('ERROR: Attempting to load JSON object from file not ending in \'.json\'');
+      return null;
+    }
+    /* Return object for opened JSON file */
+    return JSON.parse(fs.readFileSync(filepath));
+  }
+  
   return {
     
   }
