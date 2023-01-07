50945ecd209b3c5013543411d23f3a54a81d1737
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-01-06 19:00:57 -0800

    Add filepaths

--- instagram-followers.js
+++ instagram-followers.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 const InstagramFollowers = (() => {
+  /* Default filepaths */
+  const followersFilePath = './files/followers.json';
+  const followingFilePath = './files/following.json';
 
   return {
     
